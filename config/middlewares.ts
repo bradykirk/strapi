7003545b718d01cedb3599286e12789f1434a68a
Brady Kirkpatrick <bradykirkpatrick00@gmail.com>
2026-01-04 23:45:32 -0500

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://f005.backblazeb2.com',
+            'https://*.backblazeb2.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://f005.backblazeb2.com',
+            'https://*.backblazeb2.com',
+          ],
+        },
+      },
+    },
+  },
+
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
