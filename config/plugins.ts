880470a34d3de40b2d323dcadec55738db2b778e
Brady Kirkpatrick <bradykirkpatrick00@gmail.com>
2026-01-04 22:06:56 -0500

    Add pg and AWS S3 upload provider dependencies

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                accessKeyId: env(005f8195334662c0000000002),
+                secretAccessKey: env(K005aa90IbXsga7tscg75XEW2wSyawQ),
+                endpoint: env(s3.us - east-005.backblazeb2.com),
+                region: us - east-005,
+                params: {
+                    Bucket: env(hcm - website),
+                },
+            },
+        },
+    },
+});
