1deb2f0d253ca0f74cddafa638394667ecf4f406
Brady Kirkpatrick <bradykirkpatrick00@gmail.com>
2026-01-04 22:54:31 -0500

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,14 +5,15 @@ export default {
       providerOptions: {
         s3Options: {
           credentials: {
-            accessKeyId: process.env.B2_KEY_ID,
-            secretAccessKey: process.env.B2_APPLICATION_KEY,
+            accessKeyId: process.env.B2_ACCESS_KEY_ID,
+            secretAccessKey: process.env.B2_SECRET_ACCESS_KEY,
           },
           endpoint: process.env.B2_ENDPOINT,
           region: process.env.B2_REGION,
+          forcePathStyle: true,
         },
         params: {
-          Bucket: process.env.B2_BUCKET,
+          Bucket: process.env.B2_BUCKET_NAME,
         },
       },
     },
